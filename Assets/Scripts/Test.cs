d7918b472acca827657012fe3d4779c22a73b956
Needle <needle.nudl@gmail.com>
2019-01-18 21:05:59 +0300

    Initial

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class Test : SerializedMonoBehaviour {
+
+    public ITest TestField;
+    
+    public interface ITest {}
+
+    public class SomeImplementation : ITest {
+        public int Integer;
+        public GameObject Go;
+    }
+}
